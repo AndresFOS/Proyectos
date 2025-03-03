ef77e2f823cfcbbb6be1d6cc9fb4f697b205f949
AndresFOS <andresfelipeolayasuache@gmail.com>
2025-03-03 17:13:43 -0500

    Add files via upload
    Proyecto de practica con roles

---
+++ Login/src/main/java/com/mycompany/login/Login.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.login;
+
+import com.mycompany.login.igu.Logininicial;
+
+public class Login {
+
+    public static void main(String[] args) {
+        
+        Logininicial principal = new Logininicial();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+        
+    }
+}
